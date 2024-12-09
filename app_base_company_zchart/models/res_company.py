25eee97e09404820ac1a2cbe525267f9d58b3c2e
Chill <chill421@qq.com>
2024-12-09 11:24:18 +0800

    ztree zchart mods 18

---
+++ app_base_company_zchart/models/res_company.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, tools, _
+
+
+class Company(models.Model):
+    _inherit = "res.company"
+
+    _parent_store = True
+    parent_path = fields.Char(index=True)
+
+    # 注意，res.partner 有 parent_id 和 child_ids
+    # all_child_ids = fields.One2many('res.company', string='All Child Companies', compute=False)
+    #
+    # @api.depends('parent_id', 'child_ids')
+    # def _compute_all_child_ids(self):
+    #     pass
+    #
