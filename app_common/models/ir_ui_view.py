448b5418243a85b5f8f7a9047c57e841a30513d2
Chill <chill421@qq.com>
2024-12-13 16:54:37 +0800

    [app_common] v18 file_path

--- app_common/models/ir_ui_view.py
+++ app_common/models/ir_ui_view.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, models, tools, SUPERUSER_ID
-from odoo.modules.module import get_resource_path
+from odoo.tools import file_open
+from odoo.tools import misc
 from odoo.tools import view_validation
 from odoo.tools.view_validation import _relaxng_cache, validate, _validators
 from odoo.tools.safe_eval import safe_eval
@@ -15,9 +16,9 @@ def app_relaxng(view_type):
     if view_type not in _relaxng_cache:
         # tree, search 特殊
         if view_type in ['list', 'search']:
-            _file = get_resource_path('app_common', 'rng', '%s_view.rng' % view_type)
+            _file = misc.file_path('app_common/rng/%s_view.rng' % view_type)
         else:
-            _file = get_resource_path('base', 'rng', '%s_view.rng' % view_type)
+            _file = misc.file_path('base/rng/%s_view.rng' % view_type)
         with tools.file_open(_file) as frng:
             try:
                 relaxng_doc = etree.parse(frng)
